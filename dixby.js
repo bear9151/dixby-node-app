37ebee36e0f5ff7f1f6b573b571c43570400d0d7
Barrett <barrett.allen@icloud.com>
2017-04-06 23:04:29 -0400

    committing working Twitter functions

--- dixby.js
+++ dixby.js
@@ -1,8 +1,30 @@
-var myKeys = require("./keys.js"),
-    keyList = myKeys.twitterKeys,
-    twitter = require('twitter'),
-    spotify = require('spotify'),
+// Initial Global Variables
+
+var fs = require('fs'),
     request = require('request'),
-    fs = require('fs'),
+    twitter = require('twitter'),
+	myKeys = require("./keys.js"),
+    spotify = require('spotify'),
+    keyList = myKeys.twitterKeys,
     firstInput = process.argv[2],
-    secondInput = process.argv[3];
+    secondInput = process.argv[3],
+    tweetRequest = "my-tweets",
+    songRequest = "spotify-this-song",
+    movieRequest = "movie-this",
+    commandRequest = "do-what-it-says",
+    login = {screen_name: 'bear9151', count: 20},
+    client = new twitter(keyList);
+
+// Function to write tweets
+function getTweets() {
+	client.get('statuses/user_timeline', login, function(error, tweets, response) {
+	  if (!error) {
+	    for (var i = 0; i < 20; i++) {
+	    	console.log("--------------------------------------------")
+	    	console.log(tweets[i].created_at + " | " + tweets[i].text);
+	    }
+	  } else {
+	  	throw Error;
+	  }
+	});
+};
--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
-console.log('this is loaded');
+console.log('Twitter Authentication is Successful.');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'yrAIWTdA069P0nvPMLJqORTVV',
+  consumer_secret: 'qZ03bJZXKQbt77BAHPkbJMd2110BE7JKa92amzUA7GWZVD2b1h',
+  access_token_key: '44837597-s5mPGtU8NSZA0S7n6EfXouHgEpiohKlQBQricVACC',
+  access_token_secret: 'XEM1ZdWJtvB1uIL1inc5FTNPecG58GstVrHhploMBZhrv',
 }
